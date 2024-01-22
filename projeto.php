583192d9f8a5a7dd45383996b458fee67b60b83a
Tella <tellabitt12@gmail.com>
2024-01-22 17:41:23 -0300

    primeira modoficação do projeto - inserindo mais códigos

--- projeto.php
+++ projeto.php
@@ -8,6 +8,10 @@
 <body>
     <?
     $string = "Curso na DIO";
+    $numeros = 50;
+    $array = array("PHP","HTML","CSS","JAVA");
+    date_default_timezone_set('America/Sao_Paulo');
+    $data = date("d/m/Y  H:i:s");
     ?>
    <H4>  Essa é uma variável do tipo string, para implementar
 uma variável do tipo string utilizamos aspas simples('')
@@ -16,7 +20,29 @@ ou duplas("") :
 echo $string;
 
 ?> 
-
-</H4>
+</h4>
+<h4>Essa é uma variável do tipo números/integer
+    para implementar essa variável utilizamos somente 
+    os números dessa forma .
+    :
+    <?php
+    echo $numeros;
+    ?>
+</h4>
+<h4>Essa é uma variável com array, para implementar 
+    um array em uma variável utilizamos desta forma .
+    :
+    <?
+    print_r($array)
+    ?>
+</h4>
+<h4>Vamos agora implementar uma váriável do tipo datas, 
+    para implementar a variável com datas , utilizamos 
+uma função própria do PHP desta forma.
+:
+<? 
+print $data;
+?>
+</h4>
 </body>
 </html>
